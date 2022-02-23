40f0dc74c84fdf1595d406cd78c69d1172e5397c
sianida26 <chesakeselekkebo@gmail.com>
2022-02-24 03:16:49 +0700

    Section 2 fix

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import './src/css/tailwind.css'
+import './src/css/index.css'
 
 import {
   mobileMenuClose,
   mobileMenuOpen,
 } from './src/js/header.js'
+
+import feather from 'feather-icons'
+feather.replace() // Replace all feather icons with SVG
 
 document.querySelector('#mobile-menu-toggler').addEventListener('click', mobileMenuOpen)
 document.querySelector('#mobile-menu-close').addEventListener('click', mobileMenuClose)
